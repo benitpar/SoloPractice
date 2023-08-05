1b67fc520150e286142ae7d86882d39c64ec25e0
Louis Benitez <louisben1998@gmail.com>
2023-08-04 20:16:37 -0400

    First Scenario

--- src/test/java/tek/dragons/cucumber/steps/SignInSteps.java
+++ src/test/java/tek/dragons/cucumber/steps/SignInSteps.java
@@ -18,13 +18,13 @@ public class SignInSteps extends CommonUtility{
 		String actualTitle = getTitle();
 		String expectedTitle = "React App";
 		Assert.assertEquals(actualTitle, expectedTitle);
-		Assert.assertTrue(isElementDisplayed(factory.homePage().retailLogo));
+		Assert.assertTrue(isElementDisplayed(factory.signInPage().retailLogo));
 		logger.info("user is on retail website");
 	}
 	
 	@When("User click on Sign in option")
 	public void userClickOnSignInOption() {
-		click(factory.homePage().signInOption);
+		click(factory.signInPage().signInOption);
 		logger.info("user cliked on Sign In option");
 
 	}
@@ -42,46 +42,16 @@ public class SignInSteps extends CommonUtility{
 		slowDown();
 		logger.info("user clicked on login button");
 	}
-	@Then("User should be logged into Account")
-	public void userShouldBeLoggedIntoAccount() {
-		waitTillPresence(factory.homePage().accountOption);
-		Assert.assertTrue(isElementDisplayed(factory.homePage().accountOption));
-		Assert.assertTrue(isElementDisplayed(factory.homePage().ordersOption));
-		Assert.assertTrue(isElementDisplayed(factory.homePage().logOutOption));
+	@Then("User should be logged in into Account")
+	public void userShouldBeLoggedInIntoAccount() {
+		waitTillPresence(factory.signInPage().accountOption);
+		Assert.assertTrue(isElementDisplayed(factory.signInPage().accountOption));
+		Assert.assertTrue(isElementDisplayed(factory.signInPage().ordersOption));
+		Assert.assertTrue(isElementDisplayed(factory.signInPage().logOutOption));
 		logger.info("user logged in into account");
 	}
 	
-	@When("User click on Create New Account button")
-	public void userClickOnCreateNewAccountButton() {
-		click(factory.signInPage().createAcctBtn);
-		slowDown();
-		logger.info("User clicked on Create New Account Button");
-	}
-	@When("User fill the signUp information with below data {string} and {string} and {string} and {string}")
-	public void userFillTheSignUpInformationWithBelowDataAndAndAnd(String name, String email, String password, String confirmPass) {
-	   sendText(factory.signInPage().nameField,name);
-	   slowDown();
-	   sendText(factory.signInPage().emailbox,email);
-	   slowDown();
-	   sendText(factory.signInPage().passwordBox,password);
-	   slowDown();
-	   sendText(factory.signInPage().confirmPassword,confirmPass);
-	   slowDown();
-	   logger.info("User filled in sign up information");
-	}
-	@When("User click on SignUp button")
-	public void userClickOnSignUpButton() {
-		click(factory.signInPage().signupBtn);
-		slowDown();
-		logger.info("Sign up button clicked");
-	  
-	}
-	@Then("User should be logged into account page")
-	public void userShouldBeLoggedIntoAccountPage() {
-		Assert.assertTrue(isElementDisplayed(factory.signInPage().textVerify));
-		Assert.assertTrue(isElementDisplayed(factory.signInPage().cardNumberverify));
-		logger.info("User's logged in");
-	}
+	
 
 	
 	
--- src/test/resources/features/SignIn.feature
+++ src/test/resources/features/SignIn.feature
@@ -1,13 +1,14 @@
-@signIn
+
 Feature: Retail SignIn
 
-Background: 
 
+@signIn
 Scenario: Verify user can sign in into Retail Application
 
+Given User is on retail website
+When User click on Sign in option
+And User enter email 'louieben300@gmail.com' and password 'Lou@ons123'
+And User click on login button
+Then User should be logged in into Account
 
-Scenario: Verify user can create an account into Retail Website
-
-
-Examples:
 
